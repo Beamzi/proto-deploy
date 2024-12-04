4805d26bc909e5bbebd88e4f4f83edbb850727f8
James Day <159420270+Beamzi@users.noreply.github.com>
2024-12-04 11:37:42 +1000

    config merge setup

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # proto-deploy
+
+this is a webpack deploy test as well as config template repo
--- webpack.common.js
+++ webpack.common.js
@@ -1,11 +1,7 @@
-
-/*
-
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
   entry: './src/index.js',
   output: {
     filename: 'main.js',
@@ -13,11 +9,6 @@ module.exports = {
     clean: true,
   },
   devtool: 'eval-source-map',
-  devServer: {
-    static: path.join(__dirname, 'dist'),
-    port: 9000,
-    open: true,
-  },
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html', 
@@ -32,28 +23,3 @@ module.exports = {
     ],
   },
 };
-
-*/
-
-
-
-
-/*
-for image files paths
-npm install --save-dev html-loader
-
-add these 
-
-      {
-        test: /\.html$/i,
-        loader: "html-loader",
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: "asset/resource",
-      },
-
-
-*/
-
-      
